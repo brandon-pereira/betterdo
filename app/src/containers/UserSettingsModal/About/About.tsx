64f633bb3d3a91918b1070c61db3578952ef6510
Brandon Pereira <brandonpereiras@gmail.com>
2025-05-22 16:41:37 -0600

    fix path

--- app/src/containers/UserSettingsModal/About/About.tsx
+++ app/src/containers/UserSettingsModal/About/About.tsx
@@ -1,26 +1,26 @@
-import loadable from '@loadable/component';
+import loadable from "@loadable/component";
 
-import { Container, Logo, ChangeLog } from './About.styles';
+import { Container, Logo, ChangeLog } from "./About.styles";
 
-import { VERSION } from '@utilities/env';
+import { VERSION } from "@utilities/env";
 
-const ChangeLogLoader = loadable.lib(() => import('../../../../CHANGELOG.md'));
+const ChangeLogLoader = loadable.lib(
+  () => import("../../../../../CHANGELOG.md"),
+);
 
 function About() {
-    return (
-        <Container>
-            <Logo />
-            <h1>BetterDo.</h1>
-            <h2>Version {VERSION}</h2>
-            <ChangeLogLoader>
-                {({ html: changelog }) => (
-                    <ChangeLog
-                        dangerouslySetInnerHTML={{ __html: changelog }}
-                    />
-                )}
-            </ChangeLogLoader>
-        </Container>
-    );
+  return (
+    <Container>
+      <Logo />
+      <h1>BetterDo.</h1>
+      <h2>Version {VERSION}</h2>
+      <ChangeLogLoader>
+        {({ html: changelog }) => (
+          <ChangeLog dangerouslySetInnerHTML={{ __html: changelog }} />
+        )}
+      </ChangeLogLoader>
+    </Container>
+  );
 }
 
 export default About;
