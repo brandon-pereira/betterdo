d9c6f08d36bedd7d8506de5a887a0131518f08a5
Brandon Pereira <brandonpereiras@gmail.com>
2025-07-21 17:08:34 -0600

    knip fix

--- website/src/constants.ts
+++ website/src/constants.ts
@@ -1,3 +1,3 @@
-export const APP_URL = import.meta.env.PUBLIC_APP_URL ?? "http://localhost:4000";
+const APP_URL = import.meta.env.PUBLIC_APP_URL ?? "http://localhost:4000";
 
 export const LAUNCH_URL = APP_URL + "/auth/google";
